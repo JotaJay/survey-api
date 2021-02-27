ae54d54c7b8bed13612effc306fa61100fb6bb8c
Junior <reinaldo.diasjunior@gmail.com>
2021-02-26 22:49:46 -0300

    Starting jest tests

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,3 @@
-import "reflect-metadata";
-import express from "express";
-import "./database";
-
-import { router } from "./routes";
-const app = express();
-const port = 3333;
-
-app.use(express.json());
-app.use(router);
+import { app, port } from "./app";
 
 app.listen(port, () => console.log(`Running on port ${port}`));
